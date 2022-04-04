1847863d2bfe707d5151969654eb8dabd2bad3ec
xavierJara <102043945+xavierJara@users.noreply.github.com>
2022-04-04 17:42:17 +0200

    Add files via upload

---
+++ Xavier_Alicia_practica/src/xavier_alicia_practica/Xavier_Alicia_practica.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xavier_alicia_practica;
+
+/**
+ *
+ * @author xavij
+ */
+public class Xavier_Alicia_practica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("print a");
+        System.out.println("print b");
+        System.out.println("print c");
+    }
+    
+}
